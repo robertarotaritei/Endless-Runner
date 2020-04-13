0dc080e96b325aa551178af531abe7ee429f0b8c
Arotăriței Robert, R <r.arotaritei@student.fontys.nl>
2020-04-13 14:26:47 +0200

    Add Player Movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        Debug.Log(Input.GetAxis("Horizontal"));
+        transform.Translate(Input.GetAxis("Horizontal") * sidewaysForce, 0f, 0);
+    }
+}
